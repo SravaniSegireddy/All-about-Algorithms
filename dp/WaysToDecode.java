70a94a1e850b8b2fee79129ff6e4cf57a0b07296
SRAVANI SEGIREDDY <sravanisegireddy@gmail.com>
2021-03-03 11:24:09 +0800

    WaysToDecode.java

---
+++ dp/WaysToDecode.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int numDecodings(String s) {
+        if(s == null || s.length() == 0)
+            return 0;
+            
+        int M = 1000000007;
+
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for(int i = 2; i < dp.length; i += 1) {
+            if(s.charAt(i-1) != '0')
+               dp[i] = (dp[i] + dp[i-1]%M)%M;  
+
+            int twoDigit = Integer.valueOf(s.substring(i-2, i));
+            if(twoDigit >= 10 && twoDigit <= 26) 
+                dp[i] = (dp[i] + dp[i-2]%M)%M;
+        }
+        
+        return dp[s.length()];
+    }
+
+}
